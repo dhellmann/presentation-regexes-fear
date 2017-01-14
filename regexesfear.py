1d58e669f126b52dbf5a92e692b669d5aed1d995
Doug Hellmann <doug@doughellmann.com>
2017-01-14 15:45:30 -0500

    streamline example inclusion and capture

--- regexesfear.py
+++ regexesfear.py
@@ -1,28 +1,12 @@
-import contextlib
-import functools
-import io
 import re
 import sys
 
-import cogapp
-
-
-def capture(f):
-    @functools.wraps(f)
-    def wrap(*a, **kw):
-        buf = io.StringIO()
-        sys.stdout = buf
-        f(*a, **kw)
-        sys.stdout = sys.__stdout__
-        results = buf.getvalue()
-        sys.stderr.write(results)
-        return results
-    return wrap
+from output import capture
 
 
 @capture
 def showregex(s, p):
-    sys.stderr.write('\nshowregex({!r}, {!r})\n'.format(s, p))
+    print('<pre data-trim class="fragment fade-in">')
     pat = re.compile(p)
     prev = -1
     last = 0
@@ -30,9 +14,8 @@ def showregex(s, p):
         if prev < m.start():
             print(s[prev+1:m.start()], end='')
         prev = m.start()
-        print('<mark>{}</mark>'.format(s[m.start():m.end()]),
-              end='')
+        print('<mark>{}</mark>'.format(s[m.start():m.end()]), end='')
         last = m.end()
     if last:
         print(s[last:], end='')
-    print()
+    print('</pre>')
--- showcode.py
+++ showcode.py
@@ -1,22 +1,26 @@
 import cog
 import subprocess
 
+from output import capture
 
+
+@capture
 def showcode(filename, extras='data-trim'):
-    cog.out('<pre><code {}>\n'.format(extras))
+    print('<pre><code {}>\n'.format(extras))
     with open(filename, 'r', encoding='utf-8') as f:
-        cog.out(f.read())
-    cog.out('</code></pre>\n')
+        print(f.read())
+    print('</code></pre>\n')
 
 
+@capture
 def runscript(filename, extras='data-trim', fade_in=False):
-    cog.out('<pre {}'.format(extras))
+    print('<pre {}'.format(extras), end='')
     if fade_in:
-        cog.out(' class="fragment fade-in"')
-    cog.out('>\n')
+        print(' class="fragment fade-in"', end='')
+    print('>')
     result = subprocess.run(
         ['python3', filename],
         stdout=subprocess.PIPE,
     )
-    cog.out(result.stdout.decode('utf-8'))
-    cog.out('</pre>\n')
+    print(result.stdout.decode('utf-8'))
+    print('</pre>\n')
