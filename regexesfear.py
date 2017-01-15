3ba5c530c559b67c10709cfc69f805be09f32e5e
Doug Hellmann <doug@doughellmann.com>
2017-01-14 19:40:14 -0500

    clean up code a bit

--- regexesfear.py
+++ regexesfear.py
@@ -3,6 +3,13 @@ import sys
 
 from output import capture
 
+HIGHLIGHT = (
+    '<span class="fragment highlight-yellow" '
+    'data-fragment-index="1">'
+    '{}'
+    '</span>'
+)
+
 
 @capture
 def showregex(s, p, flags=0):
@@ -26,7 +33,7 @@ def showregex(s, p, flags=0):
             if prev < m.start():
                 print(s[prev:m.start()], end='')
             prev = m.end()
-            print('<span class="fragment highlight-yellow" data-fragment-index="1">{}</span>'.format(s[m.start():m.end()]), end='')
+            print(HIGHLIGHT.format(s[m.start():m.end()]), end='')
             last = m.end()
         if last:
             print(s[last:], end='')
@@ -43,30 +50,5 @@ def showregex(s, p, flags=0):
                 for j, g in enumerate(m.groups(), 1):
                     print('({}, {}) {}'.format(i, j, g))
                 print('</span>')
-
-        # Show the numbers of each match.
-        # prev = 0
-        # last = 0
-        # for i, m in enumerate(pat.finditer(s), 1):
-        #     # print(last, prev, m.start(), m.end())
-        #     print(' ' * last, end='')
-        #     if prev < m.start():
-        #         print(' ' * (m.start() - prev), end='')
-        #     prev = m.end()
-        #     for j in range(m.end() - m.start()):
-        #         print(i, end='')
-        #     print()
-        #     last = m.end()
-
-        # Show the indexes of each match.
-        # prev = 0
-        # for i, l in enumerate(s):
-        #     print('{:02d}'.format(i), l)
-        # for m in pat.finditer(s):
-        #     print(prev)
-        #     if prev < m.start():
-        #         print('P {:02d} {:02d} {}'.format(prev, m.start(), s[prev:m.start()]))
-        #     prev = m.end()
-        #     print('M {:02d} {:02d} {}'.format(m.start(), m.end(), s[m.start():m.end()]))
 
     print('</pre>')
