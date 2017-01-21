46ecd1209d6618acc4f7b7fb46d7ae1dc6c793d9
Doug Hellmann <doug@doughellmann.com>
2017-01-21 11:01:04 -0500

    support passing command line args to runscript()

--- showcode.py
+++ showcode.py
@@ -13,13 +13,13 @@ def showcode(filename, extras='data-trim'):
 
 
 @capture
-def runscript(filename, extras='data-trim data-noescape', fade_in=False):
+def runscript(filename, *args, extras='data-trim data-noescape', fade_in=False):
     print('<pre {}'.format(extras), end='')
     if fade_in:
         print(' class="fragment fade-in"', end='')
     print('>')
     result = subprocess.run(
-        ['python3', filename],
+        ['python3', filename] + list(args),
         stdout=subprocess.PIPE,
     )
     print(result.stdout.decode('utf-8').rstrip('\n'))
