c7cf9be4f4c9f117a0f520f642090fd8811c2a82
Doug Hellmann <doug@doughellmann.com>
2017-01-21 11:06:19 -0500

    add a slide showing a real parsing example

---
+++ ex2.py
@@ -0,0 +1,18 @@
+import re
+import sys
+
+_pattern = re.compile('''
+^
+(?P<ip>(\d{1,3}\.){3}\d{1,3})
+.*
+\[(?P<date>[^]]+)\]
+[^"]*
+"((?P<method>\w+)\s+
+  (?P<path>[^"]*)\s+HTTP/[^"]+)"
+\s+
+(?P<response>\d+)
+''', re.VERBOSE)
+
+for line in open('ex2.log', 'r', encoding='utf-8'):
+    parsed = _pattern.search(line).groupdict()
+    print('{ip:<15}: {path}'.format(**parsed))
