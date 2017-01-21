6b9e62c621ca10f18df4ada0da77952a2e4f3d23
Doug Hellmann <doug@doughellmann.com>
2017-01-21 12:12:05 -0500

    make sub() example a little more readable

--- ex3.py
+++ ex3.py
@@ -3,6 +3,7 @@ import re
 bold = re.compile(r'\*{2}(.*?)\*{2}')
 
 text = 'Make this **bold**.  This **too**.'
+html = bold.sub(r'<b>\1</b>', text)
 
 print('Text:', text)
-print('Bold:', bold.sub(r'<b>\1</b>', text))
+print('HTML:', html)
