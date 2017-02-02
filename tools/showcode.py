524b3dc958053dea8258d99231b3c77ba4b63280
Doug Hellmann <doug@doughellmann.com>
2017-02-02 16:46:35 -0500

    support plain files separately from code files

--- tools/showcode.py
+++ tools/showcode.py
@@ -5,6 +5,24 @@ from tools.output import capture
 
 
 @capture
+def showfile(filename, extras='data-trim data-noescape', lines=None, mark=()):
+    print('<pre {}>\n'.format(extras))
+    with open(filename, 'r', encoding='utf-8') as f:
+        body = f.readlines()
+    if lines:
+        # Editors start numbering lines at 1, so make it easier for me
+        # to pass a start and stop pair by converting from the 1
+        # indexed list to the 0 indexed list for the start value.
+        body = body[lines[0] - 1:lines[1]]
+    for i, line in enumerate(body, 1):
+        out = line.replace('<', '&lt;')
+        if i in mark:
+            out = '<mark>{}</mark>'.format(out)
+        print(out, end='')
+    print('</pre>\n')
+
+
+@capture
 def showcode(filename, extras='data-trim data-noescape', lines=None, mark=()):
     print('<pre><code {}>\n'.format(extras))
     with open(filename, 'r', encoding='utf-8') as f:
